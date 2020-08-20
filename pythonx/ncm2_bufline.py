99672ca300f584173544dc492c1a200305d95f86
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2020-08-20 10:58:42 +0200

    only autocomplete at end of line

--- pythonx/ncm2_bufline.py
+++ pythonx/ncm2_bufline.py
@@ -2,37 +2,36 @@
 
 import vim
 from ncm2 import Ncm2Source, getLogger
-import re
-from copy import deepcopy
 
 logger = getLogger(__name__)
 
 
 class Source(Ncm2Source):
-
     def on_complete(self, ctx):
+        # how many lines up/down do we complete?
+        # total, not each direction
         lines = 1000
 
-        lnum = ctx['lnum']
-        ccol = ctx['ccol']
-        b = ctx['typed'].strip()
-
-        bufnr = ctx['bufnr']
+        # current buffer for lookup
+        bufnr = ctx["bufnr"]
         buf = vim.buffers[bufnr]
 
-        matches = []
+        # where are we, what's the current line look like.
+        lnum = ctx["lnum"]
+        ccol = ctx["ccol"]
+        b = buf[lnum - 1]
+        if ccol - 1 != len(b):  # we are not at the end of the current line
+            return
+        b = b.strip()
 
-        matcher = self.matcher_get(ctx)
+        matches = [] # output buffer
 
-        matches = []
-        seen = {}
-
-        beginl = max(lnum - 1 - int(lines / 2), 0)
-        endl = lnum - 1 + int(lines / 2)
-        stepl = 1000
+        beginl = max(lnum - 1 - int(lines // 2), 0)
+        endl = lnum - 1 + int(lines // 2)
+        stepl = 1000 # how many lines at once do we at most query?
 
         for lidx in range(beginl, endl, stepl):
-            lines = buf[lidx: min(lidx + stepl, endl)]
+            lines = buf[lidx : min(lidx + stepl, endl)]
 
             # convert 0 base to 1 base
             for i, line in enumerate(lines):
@@ -42,7 +41,7 @@ class Source(Ncm2Source):
                         item = self.match_formalize(ctx, line.strip())
                         matches.append(item)
 
-        self.complete(ctx, ctx['startccol'], matches)
+        self.complete(ctx, ctx["startccol"], matches)
 
 
 source = Source(vim)
